ab953ad208fc645f26a775cbe4f1c7312fe453fd
woongblack <woongblack123@gmail.com>
2026-03-23 22:58:35 +0900

    feat: add main application class AllblueApplication

---
+++ src/main/java/com/allblue/AllblueApplication.java
@@ -0,0 +1,14 @@
+package com.allblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class AllblueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllblueApplication.class, args);
+    }
+}
